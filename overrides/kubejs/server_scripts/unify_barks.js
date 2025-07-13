c7f5b45763956136aaa7a4bcd7c5e2687efad989
Jake Humphries <34066256+JakeHumphries4@users.noreply.github.com>
2025-07-13 23:55:05 +0100

    Update unify_barks.js
    Remove custom recipe (Seems to be working without)
    Rename namespace for tag to 'mce2'
    Update the fermenting recipe to use the ID instead

--- overrides/kubejs/server_scripts/unify_barks.js
+++ overrides/kubejs/server_scripts/unify_barks.js
@@ -1,7 +1,6 @@
  ServerEvents.tags('item', event => {
 //unify tags
-    event.add('forge:barks', '#immersive_weathering:bark')
-    event.add('forge:barks', '#farmersdelight:barks')
+    event.add('mce2:barks', ['#immersive_weathering:bark', '#farmersdelight:barks'])
  })
  
 
@@ -16,25 +15,6 @@
         '#immersive_weathering:bark',
         '#forge:barks')
 
-//Pale Jane needs overriding manually
-    event.remove({type: 'brewinandchewin:fermenting', output:'brewinandchewin:pale_jane'})
-    event.custom({
-        'type': 'brewinandchewin:fermenting',
-        'basefluid': {
-            'count': 1000,
-            'fluid': 'brewinandchewin:rice_wine'
-        },
-        'experience': 1.0,
-        'ingredients': [
-            {'item': 'minecraft:honey_bottle'},
-            {'tag': 'forge:barks'},
-            {'item': 'minecraft:lily_of_the_valley'},
-            {'item': 'minecraft:sugar'}
-        ],
-        'result': {
-            'count': 1000,
-            'fluid': 'brewinandchewin:pale_jane'
-        },
-        'temperature':4
-    })
+//Pale Jane needs to be removed manually
+    event.remove({id:'bewinandchewin:fermenting/pale_jane_from_rice_wine'})
 })
